784b85ea832e0369574994312a33e6c3362e35e0
Blazej <bllazejk@gmail.com>
2020-05-04 22:20:01 +0200

    angular app

--- Apiary-organizer/src/app/app.component.spec.ts
+++ Apiary-organizer/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'Apiary-organizer'`, () => {
+  it(`should have as title 'apiary-organizer'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Apiary-organizer');
+    expect(app.title).toEqual('apiary-organizer');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('Apiary-organizer app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('apiary-organizer app is running!');
   });
 });
